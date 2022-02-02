e9f2fe067b4fe6d5513d988f88da0db5cf6e1e07
LittleParsnip <1041738178@qq.com>
2022-02-03 03:50:30 +0800

    。

--- Build/WebGL.framework.js
+++ Build/WebGL.framework.js
@@ -1295,7 +1295,7 @@ function _emscripten_asm_const_id(code, a0) {
  return ASM_CONSTS[code](a0);
 }
 STATIC_BASE = GLOBAL_BASE;
-STATICTOP = STATIC_BASE + 4677808;
+STATICTOP = STATIC_BASE + 4675904;
 __ATINIT__.push({
  func: (function() {
   __GLOBAL__sub_I_AccessibilityScriptingClasses_cpp();
@@ -3385,7 +3385,7 @@ __ATINIT__.push({
   ___emscripten_environ_constructor();
  })
 });
-var STATIC_BUMP = 4677808;
+var STATIC_BUMP = 4675904;
 Module["STATIC_BASE"] = STATIC_BASE;
 Module["STATIC_BUMP"] = STATIC_BUMP;
 var tempDoublePtr = STATICTOP;
